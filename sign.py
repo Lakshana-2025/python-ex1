9399f8731a9fb0f79fadf6e14a00de71171ff3c0
Lakshana-2025 <ravichandran04061980_bcs29@mepcoeng.ac.in>
2026-04-13 17:09:32 +0530

    Create sign.py

---
+++ sign.py
@@ -0,0 +1,7 @@
+rl=float(input("Enter a number:"))
+if rl>0:
+    print("The given number is a POSITIVE number")
+elif rl<0:
+    print("The given number is a NEGATIVE number")
+else:
+    print("The given number is ZERO")
